e6d1735723af65311763aa7a7c59d8c2a8c0056b
musfiqurofficial <musfiqurofficial1999@gmail.com>
2022-11-30 00:32:51 +0600

    jwt token

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nj6duvr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-//jew token verify
+//jwt token verify
 function verifyJWT(req, res, next) {
 
     const authHeader = req.headers.authorization;
